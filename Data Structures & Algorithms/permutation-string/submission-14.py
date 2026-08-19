ee097f06c9985c965ffcb4444985cb9b8d8debb0
SamarioLiu <Samario.Liu410@gmail.com>
2026-08-18 22:27:21 -0400

    Add: permutation-string - submission-14

---
+++ Data Structures & Algorithms/permutation-string/submission-14.py
@@ -0,0 +1,27 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        """
+        The core is the "Sliding window" in s2
+        But this is not yet real sliding window but to get a subpart by slicing.
+
+        Use the dict to solve this
+        """
+        count1 = {}
+        for c in s1:
+            count1[c] = 1 + count1.get(c, 0)
+
+        need = len(count1)  # char and corresponding number needed
+
+        for i in range(len(s2)):    # Set the start and initialize c2
+            count2, cur = {}, 0
+
+            for j in range(i, len(s2)):
+                count2[s2[j]] = 1 + count2.get(s2[j], 0) # update val
+
+                if count1.get(s2[j], 0) < count2[s2[j]]:
+                    break   # to next i-value
+                if count1.get(s2[j], 0) == count2[s2[j]]:
+                    cur += 1
+                if cur == need:
+                    return True
+        return False
